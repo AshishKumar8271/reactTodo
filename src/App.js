3281bf3ba8a91ddd4b94e7c1637a010c974d78e5
Ashish <ashishgujjar277@gmail.com>
2023-08-05 13:52:50 +0530

    grocery-bud

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import "./grocerList/grocery.css";
 
 
 // // getting data from local storage:
-// const getDataFromStroage=()=>{
-//   return JSON.parse(localStorage.getItem("groceryList"));
-// }
+const getDataFromStroage=()=>{
+  let StorageList=JSON.parse(localStorage.getItem("groceryList"));
+  if(StorageList!==[]){
+    return StorageList;
+  }
+  else{
+    return [];
+  }
+}
 
 
 const App = () => {
-  const [items, setItems] = useState([]);
-  // const [items, setItems] = useState(getDataFromStroage());
+  // const [items, setItems] = useState([]);
+  const [items, setItems] = useState(getDataFromStroage());
   const [itemName, setItemName] = useState('');
   const [editFlag, setEditFlag] = useState(false);
   const [editItem,setEditItem] = useState();
@@ -18,10 +24,10 @@ const App = () => {
   const [alertValue,setAlertValue] =useState('written just for the permanent space');
 
 
-  // //seting up items to local storage:
-  // useEffect(()=>{
-  //   localStorage.setItem("groceryList",JSON.stringify(items));
-  // },[items]);
+  //seting up items to local storage:
+  useEffect(()=>{
+    localStorage.setItem("groceryList",JSON.stringify(items));
+  },[items]);
 
 
   
